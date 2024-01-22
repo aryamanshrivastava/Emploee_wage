9c3c8e8e911dcc0c9db49f20845b1097721ec41c
aryaman <shrivastavaaryaman0207@gmail.com>
2024-01-22 15:43:48 +0530

    [aryaman]added:UC6

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee("Aryan", 1,200);
-        int check = (int) ((Math.random() * 10)) % 3;
-        if (check == 0) {
-            e1.attendance = 0;
-        } else if(check == 1){
-            e1.attendance = 1;
-        }else{
-            e1.attendance = 2;
-        }
-        switch(e1.attendance){
-            case 1:
-                System.out.println("Employee is present");
-                e1.workHour = 8;
-                break;
-            case 2:
-                System.out.println("Employee is absent");
-                break;
-            default:
-                System.out.println("Employee is part time");
-                e1.workHour = 4;
-        }
+        Employee e1 = new Employee("Aryan", 1, 200);
+        int totalWorkHours = 0;
+        int totalDaysWorked = 0;
+        while (totalWorkHours < 100 && totalDaysWorked < 20) {
+            int check = (int) ((Math.random() * 10)) % 3;
+
+            if (check == 0) {
+                e1.attendance = 0;
+            } else if (check == 1) {
+                e1.attendance = 1;
+            } else {
+                e1.attendance = 2;
+            }
+
+            switch (e1.attendance) {
+                case 1:
+                    System.out.println("Employee is present");
+                    e1.workHour = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is absent");
+                    e1.workHour = 0;
+                    break;
+                default:
+                    System.out.println("Employee is part-time");
+                    e1.workHour = 4;
+            }
+            totalWorkHours += e1.workHour;
+            totalDaysWorked++;
+            System.out.println("Day " + totalDaysWorked + " - Total Work Hours: " + totalWorkHours);
+            }
         Main m1 = new Main();
-        System.out.println("Monthly Salary: " + m1.calculateMonthlySalary(e1));
-
-
+        System.out.println("Total Salary: " + m1.calculateSalary(e1));
     }
     double calculateSalary(Employee e){
-        return e.workHour*e.wagePerHour;
-    }
-    double calculateMonthlySalary(Employee e) {
-        double dailySalary = calculateSalary(e);
-        return dailySalary * 20;
+        return e.workHour * e.wagePerHour;
     }
 }
